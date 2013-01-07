0523fbd7233172f5dae4fca43a26b881df2177a2
Aaron Ogle <atogle@gmail.com>
2013-01-06 22:19:51 -0500

    add draft support for buffered images and tile stitching

--- src/js/map.js
+++ src/js/map.js
@@ -1,8 +1,9 @@
 
   var zoom = 16,
-      buffer = 800,
+      buffer = 400,
       tileSize = 256,
       gm = new GlobalMercator(),
+      tileStitcher = tileStitcher('tiles/{z}/{x}/{y}.png', {scheme: 'tms'}),
       map = L.map('map'),
       layerUrl = 'http://{s}.tiles.mapbox.com/v3/atogle.map-vo4oycva/{z}/{x}/{y}.png',
       attribution = 'Map data &copy; OpenStreetMap contributors, CC-BY-SA <a href="http://mapbox.com/about/maps" target="_blank">Terms &amp; Feedback</a>',
@@ -13,50 +14,41 @@
     .setView([39.9524, -75.1636], 16)
     .addLayer(layer);
 
-  function getImageData(img) {
-    // Create an empty canvas element
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    // Copy the image contents to the canvas
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-
-    return ctx.getImageData(0, 0, img.width, img.height);
-  }
-
-  function to2D(array1D, size) {
+  function to2D(array1D, width) {
     var array2D = [],
         i;
 
-    for(i=0; i<size; i++) {
-      array2D[i] = Array.prototype.slice.call(array1D, i*size, i*size+size);
+    for(i=0; i<width; i++) {
+      array2D[i] = Array.prototype.slice.call(array1D, i*width, i*width+width);
     }
 
     return array2D;
   }
 
-  function draw(img, canvas, sourcePixel) {
-    console.log(img, canvas);
+  function draw(frictionCanvas, mapCanvas, sourcePixel) {
+    console.log(frictionCanvas, mapCanvas);
 
-    var ctx = canvas.getContext('2d'),
-        imageData = getImageData(img),
-        size = imageData.width,
-        data = imageData.data,
-        costRaster = [],
+    var mapCtx = mapCanvas.getContext('2d'),
+        frictionCtx = frictionCanvas.getContext('2d'),
+        w = frictionCanvas.width,
+        h = frictionCanvas.height,
+        frictionImageData = frictionCtx.getImageData(0, 0, w, h),
+        data = frictionImageData.data,
+        frictionRaster = [],
         cdData = [],
-        sourceRaster = to2D([], size),
-        maxCost = 4000,
+        sourceRaster = to2D([], w),
+        maxCost = 2000,
         row, col, i, n,
         red, green, blue, alpha, pixel,
         costDistance, cdImageData;
 
-    canvas.width = size;
-    canvas.height = size;
+    mapCanvas.width = w;
+    mapCanvas.height = h;
 
+    // Init the source raster
     sourceRaster[sourcePixel.y][sourcePixel.x] = 1;
 
+    // Init the friction raster
     row = -1;
     for(i = 0, n = data.length; i < n; i += 4) {
       red = data[i],
@@ -65,19 +57,21 @@
       alpha = data[i + 3],
       pixel = (i / 4);
 
-      if (pixel % size === 0) {
+      if (pixel % w === 0) {
         row++;
-        costRaster[row] = [];
+        frictionRaster[row] = [];
       }
 
-      costRaster[row][pixel - (row * 256)] = blue;
+      frictionRaster[row][pixel - (row * w)] = blue;
     }
 
-    costDistance = CostDistance.calculate(costRaster, sourceRaster, maxCost);
+    // Calculate the costdistance raster
+    costDistance = CostDistance.calculate(frictionRaster, sourceRaster, maxCost);
 
+    // Turn cost into pixels to display
     n=0;
-    for(row=0; row<size; row++){
-      for(col=0; col<size; col++){
+    for(row=0; row<w; row++){
+      for(col=0; col<h; col++){
         data[4*n] = 255;
         data[4*n + 1] = ((costDistance[row][col] || maxCost) / maxCost) * 255;
         data[4*n + 2] = 255;
@@ -85,26 +79,10 @@
         n++;
       }
     }
+    frictionImageData.data = data;
 
-    imageData.data = data;
-
-    ctx.putImageData(imageData, 0, 0);
-  }
-
-  function mergeImageData(imageDataArray, url, x, y, maxX, maxY, callback) {
-    var imageObj = new Image();
-    imageObj.onload = function() {
-      var imageData = getImageData(this);
-      imageDataArray.push(imageData);
-
-      console.log('trying to call callback');
-      callback(imageDataArray);
-      // draw(this, canvasLayer.canvas, {x: 128, y: 128}); //pixel);
-    };
-    imageObj.src = url;
-
-
-
+    // Draw it on the map layer
+    mapCtx.putImageData(frictionImageData, 0, 0);
   }
 
   map.on('click', function(evt) {
@@ -118,12 +96,6 @@
         neTileBoundsMeters = gm.TileBounds(neTile[0], neTile[1], zoom),
         neTileBoundsLatLng = gm.TileLatLonBounds(neTile[0], neTile[1], zoom),
 
-        xTileCnt = neTile[0] - swTile[0],
-        yTileCnt = neTile[1] - swTile[1],
-        totalTileCnt = xTileCnt * yTileCnt,
-        tileImageDataSize = tileSize * tileSize * 4,
-        mergedImageDataArraySize = tileImageDataSize * totalTileCnt,
-
         // tms = gm.MetersToTile(originMeters[0], originMeters[1], zoom),
         // tileBounds = gm.TileBounds(tms[0], tms[1], 16),
         // bounds = gm.TileLatLonBounds(tms[0], tms[1], 16),
@@ -135,8 +107,8 @@
 
     var xMetersDiff = neTileBoundsMeters[2] - swTileBoundsMeters[0],
         yMetersDiff = neTileBoundsMeters[3] - swTileBoundsMeters[1],
-        mergedSizeX = (neTile[0] - swTile[0]) * tileSize,
-        mergedSizeY = (neTile[1] - swTile[1]) * tileSize;
+        mergedSizeX = (neTile[0] - swTile[0] + 1) * tileSize,
+        mergedSizeY = (neTile[1] - swTile[1] + 1) * tileSize;
 
     pixel.x = Math.round(mergedSizeX * (originMeters[0] - swTileBoundsMeters[0]) / xMetersDiff);
     pixel.y = mergedSizeX - Math.round(mergedSizeX * (originMeters[1] - swTileBoundsMeters[1]) / yMetersDiff);
@@ -145,19 +117,13 @@
     if (canvasLayer) {
       map.removeLayer(canvasLayer);
     }
-    canvasLayer = L.imageOverlay.canvas(L.latLngBounds([swTileBoundsLatLng[0], swTileBoundsLatLng[1]],[neTileBoundsLatLng[0], neTileBoundsLatLng[1]]));
+    canvasLayer = L.imageOverlay.canvas(L.latLngBounds([swTileBoundsLatLng[0], swTileBoundsLatLng[1]],[neTileBoundsLatLng[2], neTileBoundsLatLng[3]]));
     canvasLayer.addTo(map);
 
-    var callback = _.after(totalTileCnt, function(data) {
-      console.log('should be called after all tiles are back', data);
+    tileStitcher.stitch(swTile[0], swTile[1], neTile[0], neTile[1], zoom, function(stitchedCanvas){
+      draw(stitchedCanvas, canvasLayer.canvas, pixel);
+      // canvasLayer.canvas.width = stitchedCanvas.width;
+      // canvasLayer.canvas.height = stitchedCanvas.height;
+      // canvasLayer.canvas.getContext('2d').drawImage(stitchedCanvas, 0, 0);
     });
-
-    for (tx=swTile[0]; tx<neTile[0]; tx++) {
-      for (ty=swTile[1]; ty<neTile[1]; ty++) {
-        url = 'tiles/'+zoom+'/'+tx+'/'+ty+'.png';
-
-        console.log(url);
-        mergeImageData(costImageData, url, neTile[0]-tx, neTile[1]-ty, xTileCnt, yTileCnt, callback);
-      }
-    }
   });
